d3fb37f9dd407a5ac41441e0a6a6c7c0ff14ecba
Enes Yuvarlan <nsnssoftware@gmail.com>
2024-10-16 02:47:08 +0300

    feat: add Clerk webhook for user events with Prisma

--- src/app/(routes)/(site)/page.tsx
+++ src/app/(routes)/(site)/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div className="h-[1080px] w-[1080px] flex bg-slate-900 mx-auto">
-      <div className="font-bold text-white text-5xl font-20 mx-auto my-auto">
-        Home Page
-      </div>
-    </div>
-  );
+  return <main></main>;
 }
